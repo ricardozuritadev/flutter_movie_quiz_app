6c7b8b9aa13f63054d911721137988f41bb5a183
rizuba <r.zurita@cleverpy.com>
2023-06-21 07:47:15 +0200

    Implement Quiz widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,6 @@
 import 'package:flutter/material.dart';
-
-import 'package:quizz/start_screen.dart';
+import 'package:quizz/quiz.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(colors: [
-              Color.fromARGB(255, 216, 183, 15),
-              Color.fromARGB(255, 240, 212, 101),
-            ], begin: Alignment.topLeft, end: Alignment.bottomRight),
-          ),
-          child: const StartScreen(),
-        ),
-      ),
-    ),
-  );
+  runApp(const Quiz());
 }
