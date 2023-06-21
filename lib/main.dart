5aa7830d9e398d5acc461fda736f627ba8049377
rizuba <r.zurita@cleverpy.com>
2023-06-21 07:16:27 +0200

    Create main widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
 
+import 'package:quizz/start_screen.dart';
+
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(),
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(colors: [
+              Color.fromARGB(255, 216, 183, 15),
+              Color.fromARGB(255, 240, 212, 101),
+            ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+          ),
+          child: const StartScreen(),
+        ),
+      ),
     ),
   );
 }
