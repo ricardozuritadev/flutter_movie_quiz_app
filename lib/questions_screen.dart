85a119e0751cdac8f0c0d11a84c16d1c39a3983a
rizuba <r.zurita@cleverpy.com>
2023-06-22 08:41:33 +0200

    Implement test answer and questions buttons

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -12,6 +12,20 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(context) {
-    return const Text('Questions screen');
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(''),
+          const SizedBox(
+            height: 30,
+          ),
+          ElevatedButton(onPressed: () {}, child: const Text('Answer')),
+          ElevatedButton(onPressed: () {}, child: const Text('Answer')),
+          ElevatedButton(onPressed: () {}, child: const Text('Answer'))
+        ],
+      ),
+    );
   }
 }
