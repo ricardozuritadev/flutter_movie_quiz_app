13c28423890788f8fc70e5205c98d1be443fad36
rizuba <r.zurita@cleverpy.com>
2023-06-22 08:41:51 +0200

    Create questions list

---
+++ lib/data/questions.dart
@@ -0,0 +1,102 @@
+import 'package:quizz/models/quiz_questions.dart';
+
+const questions = [
+  QuizQuestion(
+    '¿Cuál de las siguientes películas fue dirigida por Christopher Nolan?',
+    ['"Inception"', '"Avatar"', '"The Avengers"', '"Pulp Fiction"'],
+  ),
+  QuizQuestion(
+    '¿Qué actor interpreta a Tony Stark/Iron Man en el Universo Cinematográfico de Marvel?',
+    ['Chris Evans', 'Robert Downey Jr.', 'Chris Hemsworth', 'Tom Hiddleston'],
+  ),
+  QuizQuestion(
+    '¿Cuál de las siguientes películas es una obra de Stanley Kubrick?',
+    [
+      '"The Shining"',
+      '"E.T. the Extra-Terrestrial"',
+      '"Citizen Kane"',
+      '"Gone with the Wind"'
+    ],
+  ),
+  QuizQuestion(
+    '¿Quién interpreta al personaje de Batman en la trilogía de "The Dark Knight" dirigida por Christopher Nolan?',
+    ['Christian Bale', 'Robert Pattinson', 'Ben Affleck', 'Michael Keaton'],
+  ),
+  QuizQuestion(
+    '¿Cuál de las siguientes películas es una adaptación de una novela de J.R.R. Tolkien?',
+    [
+      '"Harry Potter and the Chamber of Secrets"',
+      '"The Hunger Games"',
+      '"The Lord of the Rings: The Fellowship of the Ring"',
+      '"The Matrix"'
+    ],
+  ),
+  QuizQuestion(
+    '¿Qué actor interpretó a James Bond en la película "Casino Royale"?',
+    ['Pierce Brosnan', 'Daniel Craig', 'Sean Connery', 'Roger Moore'],
+  ),
+  QuizQuestion(
+    '¿Cuál de las siguientes películas fue dirigida por Quentin Tarantino?',
+    [
+      '"The Revenant"',
+      '"Django Unchained"',
+      '"La La Land"',
+      '"The Social Network"'
+    ],
+  ),
+  QuizQuestion(
+    '¿Cuál es la película de Martin Scorsese que presenta a un corredor de bolsa corrupto interpretado por Leonardo DiCaprio?',
+    [
+      '"Goodfellas"',
+      '"The Departed"',
+      '"The Wolf of Wall Street"',
+      '"Taxi Driver"'
+    ],
+  ),
+  QuizQuestion(
+    '¿Quién interpreta al Joker en la película "Joker" de 2019?',
+    ['Joaquin Phoenix', 'Heath Ledger', 'Jack Nicholson', 'Jared Leto'],
+  ),
+  QuizQuestion(
+    '¿Cuál de las siguientes películas fue dirigida por Steven Spielberg?',
+    ['"Jurassic Park"', '"The Matrix"', '"Fight Club"', '"Avatar"'],
+  ),
+  QuizQuestion(
+    '¿Quién interpretó a Neo en la trilogía de "The Matrix"?',
+    ['Keanu Reeves', 'Tom Cruise', 'Brad Pitt', 'Hugh Jackman'],
+  ),
+  QuizQuestion(
+    '¿Cuál de las siguientes películas fue dirigida por Tim Burton?',
+    [
+      '"Edward Scissorhands"',
+      '"Forrest Gump"',
+      '"The Shawshank Redemption"',
+      '"Inception"'
+    ],
+  ),
+  QuizQuestion(
+    '¿Cuál es la película que cuenta la historia de un joven llamado Simba y su camino para convertirse en el rey de la sabana?',
+    ['"The Lion King"', '"Aladdin"', '"Mulan"', '"Coco"'],
+  ),
+  QuizQuestion(
+    '¿Cuál es la película basada en la vida de Freddie Mercury y la banda Queen?',
+    ['"Bohemian Rhapsody"', '"Rocketman"', '"Walk the Line"', '"La La Land"'],
+  ),
+  QuizQuestion(
+    '¿Qué actor interpreta a Wolverine en la franquicia de películas de X-Men?',
+    ['Hugh Jackman', 'Ryan Reynolds', 'Chris Hemsworth', 'Tom Hardy'],
+  ),
+  QuizQuestion(
+    '¿Quién interpreta a Hermione Granger en las películas de "Harry Potter"?',
+    ['Emma Watson', 'Emma Stone', 'Daisy Ridley', 'Natalie Portman'],
+  ),
+  QuizQuestion(
+    '¿Cuál de las siguientes películas fue dirigida por James Cameron?',
+    [
+      '"The Lord of the Rings: The Return of the King"',
+      '"The Dark Knight"',
+      '"The Matrix"',
+      '"Avatar"'
+    ],
+  ),
+];
