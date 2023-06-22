64ba6a417a1df525043aeaa7b4b1523a8bf0a723
rizuba <r.zurita@cleverpy.com>
2023-06-22 13:28:52 +0200

    Modify start button

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -22,6 +22,9 @@ class StartScreen extends StatelessWidget {
             onPressed: startQuiz,
             style: ElevatedButton.styleFrom(
                 backgroundColor: const Color.fromARGB(255, 111, 10, 124),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
                 textStyle: const TextStyle(fontSize: 18),
                 padding: const EdgeInsets.fromLTRB(25, 20, 25, 20)),
             icon: const Icon(Icons.arrow_right_alt),
